5f44832ffa700cab265e8a19efbcc7d0c95ec8b7
ssmg <ssmg.sg@gmail.com>
2021-02-25 17:40:28 -0600

    Create noscript tag, basic styles Add color variables and basic styles for the whole web page Create noscript tag and add noscript.css file to stylize it Add Inter font and its stylesheet

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,15 @@
-<script lang="ts">
-	export let name: string;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<h1>SSMG portfolio</h1>
+<h2>SSMG portfolio</h2>
+<h3>SSMG portfolio</h3>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid molestias
+  eligendi ipsam corporis officiis nisi enim! Perspiciatis facilis illo
+  officiis! Provident dolorum beatae adipisci ratione nihil nisi sunt quas
+  perspiciatis?
+</p>
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+  reprehenderit harum voluptates nihil aut. Minus, dicta alias vel distinctio
+  tempore labore ipsum, iusto nesciunt molestias suscipit molestiae
+  reprehenderit amet quis.
+</p>
