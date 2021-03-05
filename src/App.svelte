2aa5e150902c956fff01042960a51d95522f1334
ssmg <ssmg.sg@gmail.com>
2021-03-05 15:15:10 -0600

    Add Layout Add NavBar Add Footer Make NavBar responsive using mobile first design

--- src/App.svelte
+++ src/App.svelte
@@ -4,10 +4,60 @@
 
   // External libraries
   import { Router, Route } from 'svelte-navigator'
+  import NavBar from './components/navbar/NavBar.svelte'
+  import Footer from './components/Footer.svelte'
+  import 'boxicons'
 </script>
 
-<Router>
-  <Route path="/">
-    <Landing />
-  </Route>
-</Router>
+<div id="app">
+  <Router>
+    <NavBar />
+    <div class="middle-section">
+      <main>
+        <Route path="/">
+          <Landing />
+        </Route>
+      </main>
+      <Footer />
+    </div>
+  </Router>
+</div>
+
+<style>
+  #app {
+    height: 100vh;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr auto;
+    grid-template-areas:
+      'middle-section'
+      'navbar';
+  }
+
+  .middle-section {
+    grid-area: middle-section;
+    overflow: auto;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr auto;
+    grid-template-areas:
+      'main'
+      'footer';
+  }
+
+  main {
+    grid-area: main;
+  }
+
+  @media only screen and (min-width: 768px) {
+    #app {
+      grid-template-columns: 1fr;
+      grid-template-rows: auto 1fr;
+      grid-template-areas:
+        'navbar'
+        'middle-section';
+    }
+  }
+</style>
