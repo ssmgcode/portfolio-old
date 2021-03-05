ffbfafd243be5d914329a44c45a6774ff54c63be
ssmg <ssmg.sg@gmail.com>
2021-03-05 15:17:25 -0600

    Create BigMenu and SmallMenu Create BigMenu and Small Menu components for NavBar to be responsive

---
+++ src/components/navbar/BigMenu.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import { Link } from 'svelte-navigator'
+</script>
+
+<div class="big-menu">
+  <Link to="/education" class="menu-item"><span>Education</span></Link>
+  <Link to="/experience" class="menu-item"><span>Experience</span></Link>
+  <Link to="/projects" class="menu-item"><span>Projects</span></Link>
+  <Link to="/contact-me" class="menu-item"><span>Contact me</span></Link>
+</div>
+
+<style>
+  .big-menu {
+    display: none;
+
+    flex: 0 1 auto;
+    grid-template-columns: repeat(4, 1fr);
+    justify-items: center;
+    gap: 10px;
+  }
+
+  :global(.big-menu > .menu-item) {
+    color: rgba(var(--monochromatic-color), 0.7);
+    text-decoration: none;
+  }
+
+  :global(.big-menu > .menu-item):hover {
+    text-decoration: underline;
+  }
+
+  @media only screen and (min-width: 768px) {
+    .big-menu {
+      display: grid;
+    }
+  }
+</style>
---
+++ src/components/navbar/SmallMenu.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import { Link } from 'svelte-navigator'
+  import { slide, fade } from 'svelte/transition'
+</script>
+
+<div class="container" transition:fade={{ duration: 200 }}>
+  <div
+    class="small-menu"
+    on:click|stopPropagation
+    transition:slide={{ duration: 200 }}
+  >
+    <h3 class="title">Menu</h3>
+    <Link to="/education" class="menu-item"><p>Education</p></Link>
+    <Link to="/experience" class="menu-item"><p>Experience</p></Link>
+    <Link to="/projects" class="menu-item"><p>Projects</p></Link>
+    <Link to="/contact-me" class="menu-item"><p>Contact me</p></Link>
+  </div>
+</div>
+
+<style>
+  .container {
+    height: 100vh;
+    width: 100vw;
+    background: rgba(0, 0, 0, 0.2);
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 2;
+  }
+
+  .small-menu {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    width: 90%;
+    margin-left: auto;
+    margin-right: auto;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(5, 1fr);
+    gap: 10px;
+
+    padding: 1rem;
+    padding-bottom: 2rem;
+    background: var(--surface-color);
+    text-align: center;
+    box-shadow: var(--shadow-outside-2);
+    border-radius: var(--border-radius) var(--border-radius) 0 0;
+  }
+
+  .title {
+    margin: 0;
+  }
+
+  :global(.small-menu > .menu-item) {
+    color: rgba(var(--monochromatic-color), 0.7);
+    text-decoration: none;
+    border-radius: 6px;
+  }
+
+  :global(.small-menu > .menu-item):hover {
+    background: rgba(var(--monochromatic-color), 0.1);
+    text-decoration: none;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  @media only screen and (min-width: 768px) {
+    .container {
+      display: none;
+    }
+  }
+</style>
