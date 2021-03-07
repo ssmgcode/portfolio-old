7e5e6b42aab656cb1c85afde60cb9f346e9cc63d
ssmg <ssmg.sg@gmail.com>
2021-03-07 14:53:47 -0600

    Add LandingPage to App

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   // Components
-  import Landing from './landing/LandingPage.svelte'
+  import LandingPage from './landing/LandingPage.svelte'
 
   // External libraries
   import { Router, Route } from 'svelte-navigator'
