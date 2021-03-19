9b1100f11682ef2b48f2e660376d0911949cd2c9
ssmg <ssmg.sg@gmail.com>
2021-03-19 11:52:07 -0600

    Add link to header and implement button to toggle dark theme

--- src/components/navbar/NavBar.svelte
+++ src/components/navbar/NavBar.svelte
@@ -2,20 +2,28 @@
   // Components
   import BigMenu from './BigMenu.svelte'
   import SmallMenu from './SmallMenu.svelte'
+  import ToggleDarkTheme from './ToggleDarkTheme.svelte'
+  import ToggleDarkThemeButton from './ToggleDarkThemeButton.svelte'
+
+  // Scripts
+  import { Link } from 'svelte-navigator'
 
   let showSmallMenu: boolean = false
   const toggleShowSmallMenu = () => (showSmallMenu = !showSmallMenu)
   window.onclick = (e) => {
-    if (!e.target.matches('.burger')) showSmallMenu = false
+    if (!e.target.matches('.burger')) {
+      showSmallMenu = false
+    }
   }
 
   const navBarLinks = ['education', 'experience', 'projects', 'contact me']
 </script>
 
 <nav>
-  <h1 class="title" on:click={() => document.body.classList.toggle('dark')}>
-    SSMG
-  </h1>
+  <h1 class="title"><Link to="/">SSMG</Link></h1>
+  <ToggleDarkTheme let:isDarkThemeTurnedOn>
+    <ToggleDarkThemeButton {isDarkThemeTurnedOn} />
+  </ToggleDarkTheme>
   <BigMenu {navBarLinks} />
   <button class="burger" on:click={toggleShowSmallMenu}>
     <box-icon name="menu" size="2em" class="burger" color="var(--text-color)" />
@@ -46,7 +54,12 @@
 
   .title {
     flex: 1 1 auto;
-    font-size: 1.2em;
+    font-size: 1.5em;
+  }
+
+  nav .title > :global(a) {
+    color: inherit;
+    text-decoration: none;
   }
 
   @media only screen and (min-width: 768px) {
