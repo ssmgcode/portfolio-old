326e681b620476e120719221477f82227bea8308
ssmg <ssmg.sg@gmail.com>
2021-03-19 11:53:18 -0600

    Create logic to toggle dark theme button

---
+++ src/components/navbar/ToggleDarkTheme.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import darkThemeStore from '../../stores/dark-theme'
+
+  const toggleDarkTheme = () => {
+    darkThemeStore.toggleDarkModeTheme()
+  }
+
+  $: isDarkThemeTurnedOn = $darkThemeStore
+</script>
+
+<button on:click={toggleDarkTheme}>
+  <slot {isDarkThemeTurnedOn} />
+</button>
+
+<style>
+  button {
+    margin-right: 1em;
+  }
+</style>
