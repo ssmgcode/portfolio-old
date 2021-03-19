aea8e7ab3be2fa8186abb08b6d3555befc0f154d
ssmg <ssmg.sg@gmail.com>
2021-03-19 13:40:32 -0600

    Create grid layout for sections

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,28 @@
-<footer>Footer</footer>
+<script>
+  import FooterSection from './FooterSection.svelte'
+</script>
+
+<footer>
+  <FooterSection title="About" gridArea="about">
+    <span>Made with Svelte and TypeScript</span>
+  </FooterSection>
+  <FooterSection title="Explore" gridArea="explore" />
+  <FooterSection title="Follow" gridArea="follow" />
+</footer>
 
 <style>
   footer {
-    height: 150px;
-    background: var(--surface-color);
+    height: 450px;
+    background: var(--surface-background);
     grid-area: footer;
+
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-areas:
+      'about'
+      'explore'
+      'follow';
+    gap: 10px;
   }
 </style>
