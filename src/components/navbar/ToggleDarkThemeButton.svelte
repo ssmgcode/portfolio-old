955e121ad076218501828b38fdebab8754a22bd6
ssmg <ssmg.sg@gmail.com>
2021-03-19 11:53:26 -0600

    Create button to toggle dark theme

---
+++ src/components/navbar/ToggleDarkThemeButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let isDarkThemeTurnedOn: boolean
+</script>
+
+{#if isDarkThemeTurnedOn}
+  <box-icon name="sun" />
+{:else}
+  <box-icon name="moon" />
+{/if}
+
+<style>
+  box-icon {
+    fill: var(--text-color);
+  }
+</style>
