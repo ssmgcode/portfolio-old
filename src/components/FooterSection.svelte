357b21d9aff5c0e695b51be91dc8eca5c062da56
ssmg <ssmg.sg@gmail.com>
2021-03-19 13:40:43 -0600

    Create FooterSection

---
+++ src/components/FooterSection.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let title: string
+  export let gridArea: string
+</script>
+
+<section style="grid-area: {gridArea}">
+  <h3 class="title">{title}</h3>
+  <slot />
+</section>
+
+<style>
+  section {
+    justify-self: center;
+    align-self: center;
+    text-align: center;
+  }
+
+  .title {
+    margin: 0;
+  }
+</style>
